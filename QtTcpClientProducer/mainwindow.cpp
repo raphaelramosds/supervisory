d0e95c69fe7b62bc996a0cb51601c0bbdb344fd5
Raphael Ramos <raphael.opensource@gmail.com>
2023-08-03 10:15:16 -0300

    feat: producer interface

--- QtTcpClientProducer/mainwindow.cpp
+++ QtTcpClientProducer/mainwindow.cpp
@@ -3,41 +3,55 @@
 #include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent), ui(new Ui::MainWindow){
+    QMainWindow(parent),
+    ui(new Ui::MainWindow){
+
     ui->setupUi(this);
+
     socket = new QTcpSocket(this);
+
     tcpConnect();
 
+    /*
     connect(ui->pushButtonPut,
             SIGNAL(clicked(bool)),
             this,
             SLOT(putData()));
+    */
+
+    connect(
+        ui->horizontalSliderTiming,
+        SIGNAL(valueChanged(int)),
+        ui->labelTimingValue,
+        SLOT(setNum(int)));
 }
 
 void MainWindow::tcpConnect(){
+
     socket->connectToHost("127.0.0.1",1234);
+
     if(socket->waitForConnected(3000)){
         qDebug() << "Connected";
-    }
-    else{
+    } else{
         qDebug() << "Disconnected";
     }
 }
 
 void MainWindow::putData(){
+
     QDateTime datetime;
     QString str;
     qint64 msecdate;
 
-    if(socket->state()== QAbstractSocket::ConnectedState){
+    if(socket->state() == QAbstractSocket::ConnectedState){
 
         msecdate = QDateTime::currentDateTime().toMSecsSinceEpoch();
-        str = "set "+ QString::number(msecdate) + " " +
-              QString::number(rand()%35)+"\r\n";
+
+        str = "set "+ QString::number(msecdate) + " " + QString::number(rand()%35) + "\r\n";
 
         qDebug() << str;
-        qDebug() << socket->write(str.toStdString().c_str())
-                 << " bytes written";
+        qDebug() << socket->write(str.toStdString().c_str()) << " bytes written";
+
         if(socket->waitForBytesWritten(3000)){
             qDebug() << "wrote";
         }
